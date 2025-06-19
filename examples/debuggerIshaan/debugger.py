4b26be1ee72467330084a7a71f8564e34e0bff89
arjunvish <arjun.viswanathan23@gmail.com>
2025-06-19 15:13:38 -0400

    Comments to debugger

--- examples/debuggerIshaan/debugger.py
+++ examples/debuggerIshaan/debugger.py
@@ -1,2 +1,6 @@
-print("hellow world")
-print("Test 2")
+#Get first command line argument into a string variable
+
+#Use it to create names of SMT file, proof file, checker file, and the debug file
+
+#Create a debug file with the right name and start writing lines to it
+
